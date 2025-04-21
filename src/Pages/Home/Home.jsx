d1a7745f0fe7a8b4e9309b0b77968e47896619a7
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-04-22 02:33:19 +0600

    banner section done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Slider from './Slider';
 
 const Home = () => {
     return (
         <div>
-            This is Home
+            <Slider></Slider>
         </div>
     );
 };
---
+++ src/Pages/Home/Slider.jsx
@@ -0,0 +1,40 @@
+
+
+const Slider = () => {
+    return (
+        <div className="my-20">
+            <div className="carousel w-full md:w-3/5  h-40 md:h-80 ">
+                <div id="slide1" className="carousel-item relative w-full ">
+                    <img
+                        src="https://i.ibb.co.com/CpDwtNsj/istockphoto-1306460911-612x612.jpg"
+                        className="w-full " />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src="https://i.ibb.co.com/t6dFpB4/capm1.jpg"
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src="https://i.ibb.co.com/RGL9R307/download.jpg"
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
