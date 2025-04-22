9a0706b56ff2d66f93daa1dd9e1de0f458eadde0
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-04-22 14:34:20 +0600

    All volunteer need post section done

--- src/Pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
+++ src/Pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
@@ -6,8 +6,8 @@ const AllVolunteerNeedPosts = () => {
   const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('')
   const [isTableLayout, setIsTableLayout] = useState(true);
-  const [loader,setLoader]= useState(true)
-  
+  const [loader, setLoader] = useState(true)
+
   const handleSearch = e => {
     e.preventDefault();
     setSearch(e.target.searchTitle.value);
@@ -22,63 +22,72 @@ const AllVolunteerNeedPosts = () => {
       })
   }, [search])
   // console.log(posts)
-  
+
   return (
     <div>
-     {
-      loader &&  <span className="loading loading-spinner loading-xl"></span>
-     }
-      <p>Search post by Title</p>
-      
-      <form className="card-body" onSubmit={handleSearch}>
-        <fieldset className="fieldset">
-          {/* <label className="label">Search by title</label> */}
-          <input name='searchTitle' type="text" className="input" />
-          <button className="btn btn-neutral mt-4">Search</button>
-        </fieldset>
+      {
+        loader && <span className="loading loading-spinner loading-xl"></span>
+      }
 
+
+     
+      <form className="card-body space-y-4" onSubmit={handleSearch}>
+        <p className="text-lg font-medium">Search post by Title</p>
+
+        <div className="form-control">
+          <input
+            name="searchTitle"
+            type="text"
+            placeholder="Enter title..."
+            className="input  w-full"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-neutral">
+          Search
+        </button>
       </form>
-      
+
       <button
         onClick={() => setIsTableLayout(!isTableLayout)}
         className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
       >
         Change Layout
       </button>
-      
+
       {
         isTableLayout ? <> <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        {posts.map((post) => (
-                            <div
-                                key={post._id}
-                                className="card bg-white shadow-md hover:shadow-2xl  rounded-2xl overflow-hidden"
-                            >
-                                <figure className="h-48 overflow-hidden">
-                                    <img
-                                        src={post.thumbnail}
-                                        alt={post.title}
-                                        className="w-full h-full object-cover"
-                                    />
-                                </figure>
-                                <div className="card-body p-4">
-                                    <h2 className="card-title text-lg font-semibold mb-2">{post.title}</h2>
-                                    <div className="text-sm text-gray-600 space-y-1 mb-4">
-                                        <p>
-                                            <span className="font-medium">Category:</span> {post.category}
-                                        </p>
-                                        <p>
-                                            <span className="font-medium">Deadline:</span> {post.deadline}
-                                        </p>
-                                    </div>
-                                    <div className="card-actions justify-center ">
-                                        <Link to={`/allVolunteer/${post._id}`}>
-                                            <button className="btn btn-primary w-full">View Details</button>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div></> : <>
+          {posts.map((post) => (
+            <div
+              key={post._id}
+              className="card bg-white shadow-md hover:shadow-2xl  rounded-2xl overflow-hidden"
+            >
+              <figure className="h-48 overflow-hidden">
+                <img
+                  src={post.thumbnail}
+                  alt={post.title}
+                  className="w-full h-full object-cover"
+                />
+              </figure>
+              <div className="card-body p-4">
+                <h2 className="card-title text-lg font-semibold mb-2">{post.title}</h2>
+                <div className="text-sm text-gray-600 space-y-1 mb-4">
+                  <p>
+                    <span className="font-medium">Category:</span> {post.category}
+                  </p>
+                  <p>
+                    <span className="font-medium">Deadline:</span> {post.deadline}
+                  </p>
+                </div>
+                <div className="card-actions justify-center ">
+                  <Link to={`/allVolunteer/${post._id}`}>
+                    <button className="btn btn-primary w-full">View Details</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div></> : <>
           <div>
             <div className="overflow-x-auto">
               <table className="table">
@@ -106,7 +115,7 @@ const AllVolunteerNeedPosts = () => {
                 </tbody>
               </table>
             </div>
-            
+
           </div></>
       }
     </div>
